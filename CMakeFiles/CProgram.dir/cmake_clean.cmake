e5a37089772b96cd87a489546870b519c9ab73a2
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-08-17 09:09:08 +0530

    init commit

---
+++ CMakeFiles/CProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProgram.dir/main.c.obj"
+  "CProgram.pdb"
+  "CProgram.exe"
+  "CProgram.exe.manifest"
+  "libCProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
